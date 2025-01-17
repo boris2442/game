034085cb1dc15b0312c277bd17ac1fbb6cdc6cbe
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-17 04:41:55 +0100

    text

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,10 @@ window.onload = function () {
     document.querySelector(".loader").style.display = "none";
     document.querySelector(".loader").style.transition = "5s ease-out";
   };
+
+//   const link=document.querySelector("#beginJoke");
+//   link.addEventListener("click", (e)=>{
+//     e.preventDefault();
+
+//     window.open("index.html")
+//   })
