3c46ce38b7ff1c5650c76b7b3b90602d26037c5f
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-17 02:13:19 +0100

    javascript chargement

--- javascript/script.js
+++ javascript/script.js
@@ -15,4 +15,9 @@ function getJoke(){
     
 }
 getJoke()
-document.body.addEventListener("click", getJoke)
+document.body.addEventListener("click", getJoke);
+
+window.onload = function () {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector(".loader").style.transition = "5s ease-out";
+  };
