05669a59b45d0ae50f9b7d09a34fcde9ab11fd31
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-17 05:12:57 +0100

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -23,31 +23,3 @@ window.onload = function () {
   };
 
 
-  // Assurez-vous que Barba.js est bien chargé
-document.addEventListener('DOMContentLoaded', () => {
-    barba.init({
-      // Définir les options de Barba.js
-      transitions: [
-        {
-          // Transition entre les pages
-          name: 'default-transition',
-          once(data) {
-            // Action au premier chargement
-            console.log('Page chargée pour la première fois');
-          },
-          leave(data) {
-            // Action lors du départ de la page actuelle
-            return new Promise((resolve) => {
-              data.current.container.classList.add('fade-out');  // Applique une classe CSS pour le fade-out
-              setTimeout(resolve, 500);  // Délai pour le fade-out
-            });
-          },
-          enter(data) {
-            // Action lors de l'entrée dans la nouvelle page
-            data.next.container.classList.add('fade-in');  // Applique une classe CSS pour le fade-in
-          },
-        },
-      ],
-    });
-  });
-  
